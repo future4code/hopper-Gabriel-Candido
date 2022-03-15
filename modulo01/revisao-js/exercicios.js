f59444aadbcd710c9e1abd3dde371bf0983ab371
unknown <gabrielcandido10@hotmail.com>
2022-03-15 00:04:23 -0300

    Conclui o exercicio sete

--- modulo01/revisao-js/exercicios.js
+++ modulo01/revisao-js/exercicios.js
@@ -41,7 +41,21 @@ function retornaMaiorNumero(array) {
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
+    let maiorNumero
+    let menorNumero
+    if(num1 > num2){
+        maiorNumero = num1
+        menorNumero = num2
+    } else {
+        maiorNumero = num2
+        menorNumero = num1
+    }
 
+    return {
+        maiorNumero: maiorNumero,
+        maiorDivisivelPorMenor: maiorNumero % menorNumero === 0,
+        diferenca: maiorNumero - menorNumero
+    }
 }
 
 // EXERCÍCIO 08
