e2af208b321b590e11062d2befd2f906118aeace
unknown <gabrielcandido10@hotmail.com>
2022-04-29 01:56:19 -0300

    Ex Renderização Concluida

--- modulo03/estados-insta4/insta4/src/App.js
+++ modulo03/estados-insta4/insta4/src/App.js
@@ -10,6 +10,11 @@ const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
+const ContainerInputs = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3px;
+`;
 
 class App extends React.Component {
 
@@ -30,25 +35,67 @@ class App extends React.Component {
         foto: Bunitao,
         imagem: Bunitao,
       },
-    ]
+    ],
+
+    valorInputNome: "",
+    valorInputFoto: "",
+    valorInputImagem: ""
   }
 
-  mapeandoPost = this.state.post.map((post, index) => {
-    return (
-      <Post
-        key={index}
-        nomeUsuario={post.nome}
-        fotoUsuario={post.foto}
-        fotoPost={post.imagem}
-      />
-    )
-  })
+  onChangeNome = (event) =>{
+    this.setState({ valorInputNome: event.target.value });
+  }
+  onChangeFoto = (event) =>{
+    this.setState({ valorInputFoto: event.target.value });
+  }
+  onChangeImagem = (event) =>{
+    this.setState({ valorInputImagem: event.target.value });
+  }
+
+  onClickBtn = () =>{
+    const adicionaPost = {
+      nome: this.state.valorInputNome,
+      foto: this.state.valorInputFoto,
+      imagem: this.state.valorInputImagem
+    }
+    const novoPost = [...this.state.post, adicionaPost]
+    this.setState({post: novoPost, valorInputNome:"", valorInputFoto:"", valorInputImagem:"" })
+  }
 
   render() {
-    console.log(this.mapeandoPost)
+    
+    const mapeandoPost = this.state.post.map((post, index) => {
+      return (
+        <Post
+          key={index}
+          nomeUsuario={post.nome}
+          fotoUsuario={post.foto}
+          fotoPost={post.imagem}
+        />
+      )
+    })
+    console.log(mapeandoPost)
     return (
       <MainContainer>
-        {this.mapeandoPost}
+        <ContainerInputs>
+          <input
+            value={this.state.valorInputNome}
+            onChange={this.onChangeNome}
+            placeholder={"Nome"}
+          />
+          <input
+            value={this.state.valorInputFoto}
+            onChange={this.onChangeFoto}
+            placeholder={"Foto do Usuário"}
+          />
+          <input
+            value={this.state.valorInputImagem}
+            onChange={this.onChangeImagem}
+            placeholder={"Foto do Post"}
+          />
+          <button onClick={this.onClickBtn}>Adicionar</button>
+        </ContainerInputs>
+        {mapeandoPost}
       </MainContainer>
     );
   }
